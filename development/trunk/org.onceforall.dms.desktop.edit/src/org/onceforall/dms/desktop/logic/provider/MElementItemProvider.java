c0b347b33043a6d7396e583f9ffc02659f7f0f27
stormywatrs <stormywatrs@0d64da9a-7e37-0410-b7c2-fd8623b19026>
2007-08-12 20:54:11 +0000

    Added initial revision.

---
+++ development/trunk/org.onceforall.dms.desktop.edit/src/org/onceforall/dms/desktop/logic/provider/MElementItemProvider.java
@@ -0,0 +1,322 @@
+/*
+ * Revision History
+ * $Log: MElementItemProvider.java,v $
+ * Revision 1.2  2006/09/19 12:52:48  marc
+ * Updated to checkpoint progress.
+ *
+ */
+package org.onceforall.dms.desktop.logic.provider;
+
+
+import java.util.Collection;
+import java.util.List;
+
+
+import org.eclipse.emf.common.notify.AdapterFactory;
+import org.eclipse.emf.common.notify.Notification;
+
+import org.eclipse.emf.common.util.ResourceLocator;
+
+import org.eclipse.emf.edit.provider.ComposeableAdapterFactory;
+import org.eclipse.emf.edit.provider.IEditingDomainItemProvider;
+import org.eclipse.emf.edit.provider.IItemLabelProvider;
+import org.eclipse.emf.edit.provider.IItemPropertySource;
+import org.eclipse.emf.edit.provider.IStructuredItemContentProvider;
+import org.eclipse.emf.edit.provider.ITreeItemContentProvider;
+import org.eclipse.emf.edit.provider.ItemPropertyDescriptor;
+import org.eclipse.emf.edit.provider.ItemProviderAdapter;
+import org.eclipse.emf.edit.provider.ViewerNotification;
+import org.onceforall.dms.desktop.logic.LogicPackage;
+import org.onceforall.dms.desktop.logic.MElement;
+
+/**
+ * This is the item provider adapter for a {@link org.onceforall.dms.desktop.logic.MElement} object.
+ * <!-- begin-user-doc -->
+ * <!-- end-user-doc -->
+ * @generated
+ */
+public class MElementItemProvider
+	extends ItemProviderAdapter
+	implements	
+		IEditingDomainItemProvider,	
+		IStructuredItemContentProvider,	
+		ITreeItemContentProvider,	
+		IItemLabelProvider,	
+		IItemPropertySource {
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public static final String copyright = "Copyright 2006, Marc Maier";
+
+	/**
+	 * This constructs an instance from a factory and a notifier.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public MElementItemProvider(AdapterFactory adapterFactory) {
+		super(adapterFactory);
+	}
+
+	/**
+	 * This returns the property descriptors for the adapted class.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public List getPropertyDescriptors(Object object) {
+		if (itemPropertyDescriptors == null) {
+			super.getPropertyDescriptors(object);
+
+			addNamePropertyDescriptor(object);
+			addTypeNamePropertyDescriptor(object);
+			addTypeNameForUIPropertyDescriptor(object);
+			addDescriptionPropertyDescriptor(object);
+			addIconFilePathPropertyDescriptor(object);
+			addOverlayIconFilePathPropertyDescriptor(object);
+			addCompositeClassNamePropertyDescriptor(object);
+			addSimulatedPropertyDescriptor(object);
+		}
+		return itemPropertyDescriptors;
+	}
+
+	/**
+	 * This adds a property descriptor for the Name feature.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	protected void addNamePropertyDescriptor(Object object) {
+		itemPropertyDescriptors.add
+			(createItemPropertyDescriptor
+				(((ComposeableAdapterFactory)adapterFactory).getRootAdapterFactory(),
+				 getResourceLocator(),
+				 getString("_UI_MElement_name_feature"),
+				 getString("_UI_PropertyDescriptor_description", "_UI_MElement_name_feature", "_UI_MElement_type"),
+				 LogicPackage.Literals.MELEMENT__NAME,
+				 true,
+				 false,
+				 false,
+				 ItemPropertyDescriptor.GENERIC_VALUE_IMAGE,
+				 null,
+				 null));
+	}
+
+	/**
+	 * This adds a property descriptor for the Type Name feature.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	protected void addTypeNamePropertyDescriptor(Object object) {
+		itemPropertyDescriptors.add
+			(createItemPropertyDescriptor
+				(((ComposeableAdapterFactory)adapterFactory).getRootAdapterFactory(),
+				 getResourceLocator(),
+				 getString("_UI_MElement_typeName_feature"),
+				 getString("_UI_PropertyDescriptor_description", "_UI_MElement_typeName_feature", "_UI_MElement_type"),
+				 LogicPackage.Literals.MELEMENT__TYPE_NAME,
+				 true,
+				 false,
+				 false,
+				 ItemPropertyDescriptor.GENERIC_VALUE_IMAGE,
+				 null,
+				 null));
+	}
+
+	/**
+	 * This adds a property descriptor for the Type Name For UI feature.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	protected void addTypeNameForUIPropertyDescriptor(Object object) {
+		itemPropertyDescriptors.add
+			(createItemPropertyDescriptor
+				(((ComposeableAdapterFactory)adapterFactory).getRootAdapterFactory(),
+				 getResourceLocator(),
+				 getString("_UI_MElement_typeNameForUI_feature"),
+				 getString("_UI_PropertyDescriptor_description", "_UI_MElement_typeNameForUI_feature", "_UI_MElement_type"),
+				 LogicPackage.Literals.MELEMENT__TYPE_NAME_FOR_UI,
+				 true,
+				 false,
+				 false,
+				 ItemPropertyDescriptor.GENERIC_VALUE_IMAGE,
+				 null,
+				 null));
+	}
+
+	/**
+	 * This adds a property descriptor for the Description feature.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	protected void addDescriptionPropertyDescriptor(Object object) {
+		itemPropertyDescriptors.add
+			(createItemPropertyDescriptor
+				(((ComposeableAdapterFactory)adapterFactory).getRootAdapterFactory(),
+				 getResourceLocator(),
+				 getString("_UI_MElement_description_feature"),
+				 getString("_UI_PropertyDescriptor_description", "_UI_MElement_description_feature", "_UI_MElement_type"),
+				 LogicPackage.Literals.MELEMENT__DESCRIPTION,
+				 true,
+				 false,
+				 false,
+				 ItemPropertyDescriptor.GENERIC_VALUE_IMAGE,
+				 null,
+				 null));
+	}
+
+	/**
+	 * This adds a property descriptor for the Icon File Path feature.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	protected void addIconFilePathPropertyDescriptor(Object object) {
+		itemPropertyDescriptors.add
+			(createItemPropertyDescriptor
+				(((ComposeableAdapterFactory)adapterFactory).getRootAdapterFactory(),
+				 getResourceLocator(),
+				 getString("_UI_MElement_iconFilePath_feature"),
+				 getString("_UI_PropertyDescriptor_description", "_UI_MElement_iconFilePath_feature", "_UI_MElement_type"),
+				 LogicPackage.Literals.MELEMENT__ICON_FILE_PATH,
+				 false,
+				 false,
+				 false,
+				 ItemPropertyDescriptor.GENERIC_VALUE_IMAGE,
+				 null,
+				 null));
+	}
+
+	/**
+	 * This adds a property descriptor for the Overlay Icon File Path feature.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	protected void addOverlayIconFilePathPropertyDescriptor(Object object) {
+		itemPropertyDescriptors.add
+			(createItemPropertyDescriptor
+				(((ComposeableAdapterFactory)adapterFactory).getRootAdapterFactory(),
+				 getResourceLocator(),
+				 getString("_UI_MElement_overlayIconFilePath_feature"),
+				 getString("_UI_PropertyDescriptor_description", "_UI_MElement_overlayIconFilePath_feature", "_UI_MElement_type"),
+				 LogicPackage.Literals.MELEMENT__OVERLAY_ICON_FILE_PATH,
+				 false,
+				 false,
+				 false,
+				 ItemPropertyDescriptor.GENERIC_VALUE_IMAGE,
+				 null,
+				 null));
+	}
+
+	/**
+	 * This adds a property descriptor for the Composite Class Name feature.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	protected void addCompositeClassNamePropertyDescriptor(Object object) {
+		itemPropertyDescriptors.add
+			(createItemPropertyDescriptor
+				(((ComposeableAdapterFactory)adapterFactory).getRootAdapterFactory(),
+				 getResourceLocator(),
+				 getString("_UI_MElement_compositeClassName_feature"),
+				 getString("_UI_PropertyDescriptor_description", "_UI_MElement_compositeClassName_feature", "_UI_MElement_type"),
+				 LogicPackage.Literals.MELEMENT__COMPOSITE_CLASS_NAME,
+				 true,
+				 false,
+				 false,
+				 ItemPropertyDescriptor.GENERIC_VALUE_IMAGE,
+				 null,
+				 null));
+	}
+
+	/**
+	 * This adds a property descriptor for the Simulated feature.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	protected void addSimulatedPropertyDescriptor(Object object) {
+		itemPropertyDescriptors.add
+			(createItemPropertyDescriptor
+				(((ComposeableAdapterFactory)adapterFactory).getRootAdapterFactory(),
+				 getResourceLocator(),
+				 getString("_UI_MElement_simulated_feature"),
+				 getString("_UI_PropertyDescriptor_description", "_UI_MElement_simulated_feature", "_UI_MElement_type"),
+				 LogicPackage.Literals.MELEMENT__SIMULATED,
+				 true,
+				 false,
+				 false,
+				 ItemPropertyDescriptor.BOOLEAN_VALUE_IMAGE,
+				 null,
+				 null));
+	}
+
+	/**
+	 * This returns the label text for the adapted class.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public String getText(Object object) {
+		String label = ((MElement)object).getName();
+		return label == null || label.length() == 0 ?
+			getString("_UI_MElement_type") :
+			getString("_UI_MElement_type") + " " + label;
+	}
+
+	/**
+	 * This handles model notifications by calling {@link #updateChildren} to update any cached
+	 * children and by creating a viewer notification, which it passes to {@link #fireNotifyChanged}.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public void notifyChanged(Notification notification) {
+		updateChildren(notification);
+
+		switch (notification.getFeatureID(MElement.class)) {
+			case LogicPackage.MELEMENT__NAME:
+			case LogicPackage.MELEMENT__TYPE_NAME:
+			case LogicPackage.MELEMENT__TYPE_NAME_FOR_UI:
+			case LogicPackage.MELEMENT__NAME_FOR_UI:
+			case LogicPackage.MELEMENT__DESCRIPTION:
+			case LogicPackage.MELEMENT__DESCRIPTION_FOR_UI:
+			case LogicPackage.MELEMENT__ICON_FILE_PATH:
+			case LogicPackage.MELEMENT__OVERLAY_ICON_FILE_PATH:
+			case LogicPackage.MELEMENT__COMPOSITE_CLASS_NAME:
+			case LogicPackage.MELEMENT__SIMULATED:
+				fireNotifyChanged(new ViewerNotification(notification, notification.getNotifier(), false, true));
+				return;
+		}
+		super.notifyChanged(notification);
+	}
+
+	/**
+	 * This adds to the collection of {@link org.eclipse.emf.edit.command.CommandParameter}s
+	 * describing all of the children that can be created under this object.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	protected void collectNewChildDescriptors(Collection newChildDescriptors, Object object) {
+		super.collectNewChildDescriptors(newChildDescriptors, object);
+	}
+
+	/**
+	 * Return the resource locator for this item provider's resources.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public ResourceLocator getResourceLocator() {
+		return DMSDesktopEditPlugin.INSTANCE;
+	}
+
+}
---
+++ development/trunk/org.onceforall.dms.desktop.edit/src/org/onceforall/dms/desktop/logic/provider/MFtpStepItemProvider.java
@@ -0,0 +1,284 @@
+/**
+ * <copyright>
+ * </copyright>
+ *
+ * $Id: MFtpStepItemProvider.java,v 1.1 2007/05/12 11:00:48 marc Exp $
+ */
+package org.onceforall.dms.desktop.logic.provider;
+
+
+import java.util.Collection;
+import java.util.List;
+
+import org.eclipse.emf.common.notify.AdapterFactory;
+import org.eclipse.emf.common.notify.Notification;
+
+import org.eclipse.emf.common.util.ResourceLocator;
+
+import org.eclipse.emf.ecore.EStructuralFeature;
+
+import org.eclipse.emf.edit.provider.ComposeableAdapterFactory;
+import org.eclipse.emf.edit.provider.IEditingDomainItemProvider;
+import org.eclipse.emf.edit.provider.IItemLabelProvider;
+import org.eclipse.emf.edit.provider.IItemPropertySource;
+import org.eclipse.emf.edit.provider.IStructuredItemContentProvider;
+import org.eclipse.emf.edit.provider.ITreeItemContentProvider;
+import org.eclipse.emf.edit.provider.ItemPropertyDescriptor;
+import org.eclipse.emf.edit.provider.ViewerNotification;
+
+import org.onceforall.dms.desktop.logic.LogicFactory;
+import org.onceforall.dms.desktop.logic.LogicPackage;
+import org.onceforall.dms.desktop.logic.MFtpStep;
+
+/**
+ * This is the item provider adapter for a {@link org.onceforall.dms.desktop.logic.MFtpStep} object.
+ * <!-- begin-user-doc -->
+ * <!-- end-user-doc -->
+ * @generated
+ */
+public class MFtpStepItemProvider
+	extends MStepItemProvider
+	implements	
+		IEditingDomainItemProvider,	
+		IStructuredItemContentProvider,	
+		ITreeItemContentProvider,	
+		IItemLabelProvider,	
+		IItemPropertySource {
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public static final String copyright = "Copyright 2006, Marc Maier";
+
+	/**
+	 * This constructs an instance from a factory and a notifier.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public MFtpStepItemProvider(AdapterFactory adapterFactory) {
+		super(adapterFactory);
+	}
+
+	/**
+	 * This returns the property descriptors for the adapted class.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public List getPropertyDescriptors(Object object) {
+		if (itemPropertyDescriptors == null) {
+			super.getPropertyDescriptors(object);
+
+			addFtpServerUrlParameterPropertyDescriptor(object);
+			addFtpUserNameParameterPropertyDescriptor(object);
+			addFtpUserPasswordParameterPropertyDescriptor(object);
+		}
+		return itemPropertyDescriptors;
+	}
+
+	/**
+	 * This adds a property descriptor for the Ftp Server Url Parameter feature.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	protected void addFtpServerUrlParameterPropertyDescriptor(Object object) {
+		itemPropertyDescriptors.add
+			(createItemPropertyDescriptor
+				(((ComposeableAdapterFactory)adapterFactory).getRootAdapterFactory(),
+				 getResourceLocator(),
+				 getString("_UI_MFtpStep_ftpServerUrlParameter_feature"),
+				 getString("_UI_PropertyDescriptor_description", "_UI_MFtpStep_ftpServerUrlParameter_feature", "_UI_MFtpStep_type"),
+				 LogicPackage.Literals.MFTP_STEP__FTP_SERVER_URL_PARAMETER,
+				 false,
+				 false,
+				 false,
+				 ItemPropertyDescriptor.GENERIC_VALUE_IMAGE,
+				 null,
+				 null));
+	}
+
+	/**
+	 * This adds a property descriptor for the Ftp User Name Parameter feature.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	protected void addFtpUserNameParameterPropertyDescriptor(Object object) {
+		itemPropertyDescriptors.add
+			(createItemPropertyDescriptor
+				(((ComposeableAdapterFactory)adapterFactory).getRootAdapterFactory(),
+				 getResourceLocator(),
+				 getString("_UI_MFtpStep_ftpUserNameParameter_feature"),
+				 getString("_UI_PropertyDescriptor_description", "_UI_MFtpStep_ftpUserNameParameter_feature", "_UI_MFtpStep_type"),
+				 LogicPackage.Literals.MFTP_STEP__FTP_USER_NAME_PARAMETER,
+				 false,
+				 false,
+				 false,
+				 ItemPropertyDescriptor.GENERIC_VALUE_IMAGE,
+				 null,
+				 null));
+	}
+
+	/**
+	 * This adds a property descriptor for the Ftp User Password Parameter feature.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	protected void addFtpUserPasswordParameterPropertyDescriptor(Object object) {
+		itemPropertyDescriptors.add
+			(createItemPropertyDescriptor
+				(((ComposeableAdapterFactory)adapterFactory).getRootAdapterFactory(),
+				 getResourceLocator(),
+				 getString("_UI_MFtpStep_ftpUserPasswordParameter_feature"),
+				 getString("_UI_PropertyDescriptor_description", "_UI_MFtpStep_ftpUserPasswordParameter_feature", "_UI_MFtpStep_type"),
+				 LogicPackage.Literals.MFTP_STEP__FTP_USER_PASSWORD_PARAMETER,
+				 false,
+				 false,
+				 false,
+				 ItemPropertyDescriptor.GENERIC_VALUE_IMAGE,
+				 null,
+				 null));
+	}
+
+	/**
+	 * This specifies how to implement {@link #getChildren} and is used to deduce an appropriate feature for an
+	 * {@link org.eclipse.emf.edit.command.AddCommand}, {@link org.eclipse.emf.edit.command.RemoveCommand} or
+	 * {@link org.eclipse.emf.edit.command.MoveCommand} in {@link #createCommand}.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public Collection getChildrenFeatures(Object object) {
+		if (childrenFeatures == null) {
+			super.getChildrenFeatures(object);
+			childrenFeatures.add(LogicPackage.Literals.MFTP_STEP__MFTP_SERVER_URL_PARAMETER);
+			childrenFeatures.add(LogicPackage.Literals.MFTP_STEP__MFTP_USER_NAME_PARAMETER);
+			childrenFeatures.add(LogicPackage.Literals.MFTP_STEP__MFTP_USER_PASSWORD_PARAMETER);
+		}
+		return childrenFeatures;
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	protected EStructuralFeature getChildFeature(Object object, Object child) {
+		// Check the type of the specified child object and return the proper feature to use for
+		// adding (see {@link AddCommand}) it as a child.
+
+		return super.getChildFeature(object, child);
+	}
+
+	/**
+	 * This returns MFtpStep.gif.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public Object getImage(Object object) {
+		return overlayImage(object, getResourceLocator().getImage("full/obj16/MFtpStep"));
+	}
+
+	/**
+	 * This returns the label text for the adapted class.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public String getText(Object object) {
+		String label = ((MFtpStep)object).getName();
+		return label == null || label.length() == 0 ?
+			getString("_UI_MFtpStep_type") :
+			getString("_UI_MFtpStep_type") + " " + label;
+	}
+
+	/**
+	 * This handles model notifications by calling {@link #updateChildren} to update any cached
+	 * children and by creating a viewer notification, which it passes to {@link #fireNotifyChanged}.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public void notifyChanged(Notification notification) {
+		updateChildren(notification);
+
+		switch (notification.getFeatureID(MFtpStep.class)) {
+			case LogicPackage.MFTP_STEP__FTP_SERVER_URL_PARAMETER:
+			case LogicPackage.MFTP_STEP__FTP_USER_NAME_PARAMETER:
+			case LogicPackage.MFTP_STEP__FTP_USER_PASSWORD_PARAMETER:
+				fireNotifyChanged(new ViewerNotification(notification, notification.getNotifier(), false, true));
+				return;
+			case LogicPackage.MFTP_STEP__MFTP_SERVER_URL_PARAMETER:
+			case LogicPackage.MFTP_STEP__MFTP_USER_NAME_PARAMETER:
+			case LogicPackage.MFTP_STEP__MFTP_USER_PASSWORD_PARAMETER:
+				fireNotifyChanged(new ViewerNotification(notification, notification.getNotifier(), true, false));
+				return;
+		}
+		super.notifyChanged(notification);
+	}
+
+	/**
+	 * This adds to the collection of {@link org.eclipse.emf.edit.command.CommandParameter}s
+	 * describing all of the children that can be created under this object.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	protected void collectNewChildDescriptors(Collection newChildDescriptors, Object object) {
+		super.collectNewChildDescriptors(newChildDescriptors, object);
+
+		newChildDescriptors.add
+			(createChildParameter
+				(LogicPackage.Literals.MFTP_STEP__MFTP_SERVER_URL_PARAMETER,
+				 LogicFactory.eINSTANCE.createMParameter()));
+
+		newChildDescriptors.add
+			(createChildParameter
+				(LogicPackage.Literals.MFTP_STEP__MFTP_USER_NAME_PARAMETER,
+				 LogicFactory.eINSTANCE.createMParameter()));
+
+		newChildDescriptors.add
+			(createChildParameter
+				(LogicPackage.Literals.MFTP_STEP__MFTP_USER_PASSWORD_PARAMETER,
+				 LogicFactory.eINSTANCE.createMParameter()));
+	}
+
+	/**
+	 * This returns the label text for {@link org.eclipse.emf.edit.command.CreateChildCommand}.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public String getCreateChildText(Object owner, Object feature, Object child, Collection selection) {
+		Object childFeature = feature;
+		Object childObject = child;
+
+		boolean qualify =
+			childFeature == LogicPackage.Literals.MFTP_STEP__MFTP_SERVER_URL_PARAMETER ||
+			childFeature == LogicPackage.Literals.MFTP_STEP__MFTP_USER_NAME_PARAMETER ||
+			childFeature == LogicPackage.Literals.MFTP_STEP__MFTP_USER_PASSWORD_PARAMETER;
+
+		if (qualify) {
+			return getString
+				("_UI_CreateChild_text2",
+				 new Object[] { getTypeText(childObject), getFeatureText(childFeature), getTypeText(owner) });
+		}
+		return super.getCreateChildText(owner, feature, child, selection);
+	}
+
+	/**
+	 * Return the resource locator for this item provider's resources.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public ResourceLocator getResourceLocator() {
+		return DMSDesktopEditPlugin.INSTANCE;
+	}
+
+}
